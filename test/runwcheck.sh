0ffee9db59ec21a6c63011e9c79fbded408bbd81
poorna <poorna2152@gmail.com>
2022-02-27 08:30:50 +0530

    Update node flags

--- test/runwcheck.sh
+++ test/runwcheck.sh
@@ -18,7 +18,7 @@ if test $kind == p; then
     $("./$1" >/dev/null 2>"$out")
     head -n 2 $out | sed -e 's/.*bal:/'"$balFile"':/' | diff -u "$2" -
 else
-    node ../../../runtime/js/main.js $1 --import > "$out"
+    node ../../../runtime/js/main.js $1 --import --experimental-wasm-eh > "$out"
     diff -u "$2" "$out"
 fi
 
